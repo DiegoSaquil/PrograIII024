f13f4deb6162b89ba477f584899512d70e885b5f
DiegoSaquil <dsaquilg@miumg.edu.gt>
2024-01-24 16:27:38 -0600

    Para progra 3

---
+++ prueba 23 de enero.py
@@ -0,0 +1,23 @@
+edad = 20
+nombre = "Diego"
+caracter = '@'
+boleano = True 
+flotante = 23.2
+
+
+a = 10
+b = 5
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a * b
+potencia = a ** b
+
+print ("las operaciones aritmeticas de python dan los siguientes resultados" "para suma:", suma ,"para resta:" ,resta ,"para multiplicacion:", multiplicacion, "para división" ,division, "para potencia" ,potencia,)
+
+sumaTiposvariados = edad + int(nombre)
+print ("tipos" ,sumaTiposvariados,)
+
+
+"""no entiendo los modulos xd jaja lol"""
+
