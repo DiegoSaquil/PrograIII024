c5ceb73557f79cb0069c0dffb7890ddaa39b8957
DiegoSaquil <dsaquilg@miumg.edu.gt>
2024-01-24 21:42:18 -0600

    ejercicio 5

---
+++ identificar.py
@@ -0,0 +1,20 @@
+dato = input("Ingrese cualquier dato: ")
+
+try:
+    num = eval(dato)
+    var = type(num).__name__
+    print("La entrada es de tipo:", var)
+except:
+    var = type(dato).__name__
+    print("La entrada es de tipo:", var)
+
+if var == "int":
+    print("¡La entrada es un número entero!")
+elif var == "float":
+    print("¡La entrada es un número decimal!")
+elif var == "str":
+    print("¡La entrada es una cadena de texto!")
+elif var == "bool":
+    print("¡La entrada es de tipo booleana!")
+else:
+    print("¡Tipo de dato no identificado!")
