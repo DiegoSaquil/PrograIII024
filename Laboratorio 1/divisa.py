c9711b51d06f06091a1662ff85523d00c637ae6e
DiegoSaquil <dsaquilg@miumg.edu.gt>
2024-01-26 12:19:50 -0600

    Laboratorio 1

--- Laboratorio 1/divisa.py
+++ Laboratorio 1/divisa.py
@@ -9,4 +9,3 @@ ingresodolar = float(input("ingrese la cantidad de dolares para la conversión: 
 quetzalesoutput = conversion_de_moneda(ingresodolar,cambioactual)
 
 print(f"{ingresodolar} dolares equivalen a {quetzalesoutput} Quetzales con un cambio de: {cambioactual}")
-
