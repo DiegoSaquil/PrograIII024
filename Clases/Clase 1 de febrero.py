64b70ae8f045d8d8f2dff77f1259edc978a9c4b7
DiegoSaquil <dsaquilg@miumg.edu.gt>
2024-02-01 11:47:44 -0600

    Clases

---
+++ Clases/Clase 1 de febrero.py
@@ -0,0 +1,30 @@
+#entrada=input("Ingresa los numeros a ordenar separados por espacios: ")
+
+#listadenumeros= [float(x)for x in entrada.split()]
+#listadenumeros.sort()
+#print(listadenumeros)
+
+#ejemplo if anidado
+#clima_frio = True
+#temperatura = -5
+
+#if temperatura < 0:
+ #   print("ESTA HELADO")
+
+  #  if clima_frio:
+   #     print("Abrigate bien")
+#print("ES HORA DE BEBER ALGO CALIENTE")
+
+#Ejemplo de arbol binario
+#class Node:
+#    def __init__(self,valor):
+#        self.valor = valor
+#        self.izquierdo = Node
+#        self.derecho = Node
+#raiz = Node(1)
+#raiz.izquierdo = Node(2)
+#raiz.derecho = Node(3)
+#raiz.izquierdo.izquierdo = Node(4)
+#raiz.izquierdo.izquierdo.izquierdo = Node(5)
+
+
